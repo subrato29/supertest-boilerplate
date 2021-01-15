1c2281299a67028ae533ed814ca57359ab67fb48
Subrato Sarkar <subratosarkar@Subratos-MacBook-Pro.local>
2021-01-15 13:50:20 -0500

    first commit

---
+++ test/README.md
@@ -0,0 +1,20 @@
+Getting started :
+------------------------------
+
+```
+Make sure node is installed.
+
+git clone 'repo-url'
+
+cd protractor-boilerplate-ebay
+
+npm install
+
+```
+
+Running the tests:
+-------------------
+```
+npm run test
+
+For running locally, start webdriver by issuing command "npm run start" in daemon mode.
---
+++ test/playlists.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import supertest from 'supertest';
+
+const baseUrl = require('../config/urls.js');
+let apiUtils = require('../lib/apiUtils.js');
+const request = supertest(baseUrl.url.baseUrl);
+
+describe('Playlists', () => {
+    it('GET /Get a List of Current Users Playlists', () => {
+        const endpoint = '/v1/me/playlists';
+        apiUtils.get(request, endpoint);
+    });
+})
