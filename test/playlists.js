fd423e74d6415b16ba1169796331f8f4ceedb19c
Subrato Sarkar <subratosarkar@Subratos-MacBook-Pro.local>
2021-01-15 14:41:56 -0500

    updated code

--- test/playlists.js
+++ test/playlists.js
@@ -5,10 +5,13 @@ import supertest from 'supertest';
 const baseUrl = require('../config/urls.js');
 let apiUtils = require('../lib/apiUtils.js');
 const request = supertest(baseUrl.baseUrl);
+const commonUtils = require('../lib/commonUtils.js')
 
 describe('Playlists', () => {
     it('GET /Get a List of Current Users Playlists', () => {
         const endpoint = '/v1/me/playlists';
-        apiUtils.get(request, endpoint);
+        apiUtils.get(request, endpoint).then((response) => {
+            console.log(response);
+        })
     });
 })
