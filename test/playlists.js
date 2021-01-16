c33d84295654aee246b419d9d883dde6a25abc08
Subrato Sarkar <subratosarkar@Subratos-MacBook-Pro.local>
2021-01-16 02:06:38 -0500

    updated code

--- test/playlists.js
+++ test/playlists.js
@@ -8,13 +8,12 @@ let apiUtils = require('../lib/apiUtils.js');
 const request = supertest(urls.baseUrl);
 
 describe('Playlists', () => {
-    it('GET /Get a List of Current Users Playlists', (done) => {
+    it('GET /Get a List of Current Users Playlists', () => {
         const endpoint = urls.endpoint.playlists.list_of_curr_users_playlist;
-        apiUtils.get(request, endpoint).then((response) => {
+        return apiUtils.get(request, endpoint).then((response) => {
              let total = response.body.total;
              console.log ('total is: ' + total);
-             expect(total).to.be.greaterThan(10); 
-             done();
+             return expect(total).to.be.greaterThan(10); 
         }).catch((err) => {
             return Promise.reject(err);
         })
